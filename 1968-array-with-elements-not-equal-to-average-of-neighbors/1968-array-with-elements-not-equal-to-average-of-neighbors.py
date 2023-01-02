268a5194553b3ba6cebfcc68dc8071ad961b2bd7
salasjr <110789948+salasjr@users.noreply.github.com>
2023-01-03 00:11:07 +0300

    Time: 5389 ms (5.17%), Space: 29.9 MB (63.57%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -4,13 +4,7 @@ class Solution:
             pre = nums[i-1]
             current = nums[i]
             next = nums[i+1]
-            
-            # If block will run when we meet 1 2 3 or 6 4 2
             if (pre < current < next) or (pre > current > next):
-                # Swap next and current
-                # For example: 
-                # 1 2 3 -> 1 3 2
-                # 6 4 2 -> 6 2 4
                 nums[i+1], nums[i] = nums[i], nums[i+1]
                 
         return nums
