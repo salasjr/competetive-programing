5253854b1694003b1d5d85ee42e90672fb93c98f
salasjr <110789948+salasjr@users.noreply.github.com>
2023-01-02 23:55:28 +0300

    Time: 768 ms (34.83%), Space: 22.6 MB (77.53%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -5,16 +5,7 @@ class Solution:
         ans = nm[len(nm)-k]
         return str(ans)
         
-        
-        # sorted(nums)
-        # m = len(nums)
-        # if m==k:
-        #     return nums[0]
-        # elif m== k+1:
-        #     return nums[1]
-        # else:
-        #     return nums[m-k]
-            
+
             
                 
         
