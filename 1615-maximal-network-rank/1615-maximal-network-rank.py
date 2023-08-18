ba7b7c927dc5150e46120e93eb37b10ed70acee2
salahadin juhar <110789948+salasjr@users.noreply.github.com>
2023-08-18 16:45:22 +0300

    Time: 359 ms (43.19%), Space: 17.9 MB (83.19%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,18 +1,32 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        adjacency_list = [[] for _ in range(n)]
-    
-        for road in roads:
-            city1, city2 = road
-            adjacency_list[city1].append(city2)
-            adjacency_list[city2].append(city1)
-
+        adjst_list = collections.defaultdict(list) 
+        for x, y in roads:
+            adjst_list[x].append(y)
+            adjst_list[y].append(x)
         max_rank = 0
         for i in range(n):
-            for j in range(i + 1, n):
-                rank = len(adjacency_list[i]) + len(adjacency_list[j])
-                if j in adjacency_list[i] or i in adjacency_list[j]:
+            for j in range(i+1,n):
+                rank = len(adjst_list[i]) + len(adjst_list[j])
+                if j in adjst_list[i] or i in adjst_list[j]:
                     rank -= 1
                 max_rank = max(max_rank, rank)
 
         return max_rank
+                
+#         adjist_list = [[] for _ in range(n)]
+    
+#         for x,y roads:
+#             city1, city2 = road
+#             adjacency_list[city1].append(city2)
+#             adjacency_list[city2].append(city1)
+
+#         max_rank = 0
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 rank = len(adjacency_list[i]) + len(adjacency_list[j])
+#                 if j in adjacency_list[i] or i in adjacency_list[j]:
+#                     rank -= 1
+#                 max_rank = max(max_rank, rank)
+
+#         return max_rank
