c4f4308828e3275ab75dc1069bb3ce15b544bf31
salahadin juhar <110789948+salasjr@users.noreply.github.com>
2023-09-30 15:53:45 +0300

    Time: 50 ms (56.83%), Space: 16.4 MB (37.94%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(1, n // 2 + 1):
             if n % i == 0:
                 substring = s[:i]
-                print(substring)
                 if substring * (n // i) == s:
                     return True
         return False
