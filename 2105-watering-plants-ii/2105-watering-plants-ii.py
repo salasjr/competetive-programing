69a535a3df2f294bb71da30011bb879300d9688e
salahadin juhar <110789948+salasjr@users.noreply.github.com>
2023-10-22 10:22:28 +0300

    Time: 674 ms (93.49%), Space: 31.6 MB (19.53%) - LeetHub

---
+++ 2105-watering-plants-ii/2105-watering-plants-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minimumRefill(self, plants: List[int], capacityA: int, capacityB: int) -> int:
+        ans = 0
+        i = 0
+        aliceWater = capacityA
+        bobWater = capacityB
+        j = len(plants) - 1
+
+        while i < j:
+            if plants[i] > aliceWater:
+                ans += 1
+                aliceWater = capacityA
+
+            if plants[j] > bobWater:
+                ans += 1
+                bobWater = capacityB
+            aliceWater -= plants[i]
+            bobWater -= plants[j]
+            i += 1
+            j -= 1
+
+        if plants[j] > bobWater and plants[i] > aliceWater and i == j:
+            ans += 1
+            bobWater = capacityB
+
+        return ans
+
+
+       
+        
+        
+        
+        
+        
+    
+
+        
