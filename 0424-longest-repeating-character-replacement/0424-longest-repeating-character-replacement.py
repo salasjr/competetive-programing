9d6c6464065ed1d1f5eeccf042a8286b9ce1e1d0
salahadin juhar <110789948+salasjr@users.noreply.github.com>
2023-11-08 16:54:43 +0300

    Time: 99 ms (62.69%), Space: 16.4 MB (13.28%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,7 +5,6 @@ class Solution:
         left =0
         for right in range(len(s)):
             count[s[right]]+=1
-            print(count)
             if (right-left+1)-max(count.values())>k:
                 count[s[left]]-=1
                 left+=1
