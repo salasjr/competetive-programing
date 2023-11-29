a6d954d21542a95b70e47cc355cb14269be99e47
salahadin juhar <110789948+salasjr@users.noreply.github.com>
2023-11-29 18:55:10 +0300

    Added solution for Palindrome Number

---
+++ A2sv/palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if( x < 0):
+            return False
+        number = x
+        reverse = 0
+        while(number > 0):
+            y = number % 10
+            number = number//10
+            reverse = reverse*10 + y
+        return reverse == x
+      
+        
+        
